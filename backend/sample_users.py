6502ab4a8e8dbdf857ff2d7e679473a86319b679
Rahul Dani <rahuls.dani@gmail.com>
2025-02-08 04:54:19 -0500

    moved backend to folder

---
+++ backend/sample_users.py
@@ -0,0 +1,22 @@
+sample_user1 = {
+    'username': '15dani1',
+    'name': 'Rahul Dani',
+    'password': 'rdani$123',
+    'email': 'rdani@andrew.cmu.edu',
+    'routes': ['Pittsburgh', 'NYC'],
+    'history': ['Los Angeles', 'San Francisco'],
+    'saves': ['Chicago', 'Seattle'],
+    'favorites': ['Miami', 'Boston']
+}
+
+sample_user2 = {
+    'username': 'nikkilovessscats',
+    'name': 'Nikki Chizmar',
+    'password': 'simba$123',
+    'email': 'nchizmar@andrew.cmu.edu',
+    'routes': ['Los Angeles', 'San Francisco'],
+    'history': ['Pittsburgh', 'NYC'],
+    'saves': ['Chicago', 'Seattle'],
+    'favorites': ['Miami', 'Boston']
+}
+
