5fe913101ff4d849dc72de2731cb64cd9450891e
Rahul Dani <rahuls.dani@gmail.com>
2025-02-08 04:49:29 -0500

    Multiple users

--- aws.py
+++ aws.py
@@ -2,6 +2,7 @@ import boto3
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 from dotenv import load_dotenv
 import os
+from sample_users import sample_user1, sample_user2
 
 # Load environment variables from .env file
 load_dotenv()
@@ -20,17 +21,39 @@ def connect_to_aws():
         print(f"Credentials error: {e}")
         return None
 
-def write_message_to_dynamodb(table_name, message):
+def write_message_to_dynamodb(table_name):
     dynamodb = connect_to_aws()
     if dynamodb:
         table = dynamodb.Table(table_name)
-        response = table.put_item(
-            Item={
-                'id': '1',  # Ensure this matches the primary key attribute name in your DynamoDB table
-                'Message': message
-            }
+        response1 = table.put_item(
+            Item=sample_user1
         )
-        return response
+        response2 = table.put_item(
+            Item=sample_user2
+        )
+        return response1, response2
+    else:
+        print("Failed to connect to DynamoDB")
+        return None
+
+def read_message_from_dynamodb(table_name, username):
+    dynamodb = connect_to_aws()
+    if dynamodb:
+        table = dynamodb.Table(table_name)
+        try:
+            response = table.get_item(
+                Key={
+                    'username': username  # Ensure this matches the primary key attribute name in your DynamoDB table
+                }
+            )
+            if 'Item' in response:
+                return response['Item']
+            else:
+                print(f"No item found with username: {username}")
+                return None
+        except Exception as e:
+            print(f"Error reading from DynamoDB: {e}")
+            return None
     else:
         print("Failed to connect to DynamoDB")
         return None
@@ -38,7 +61,13 @@ def write_message_to_dynamodb(table_name, message):
 # Example usage
 if __name__ == "__main__":
     table_name = 'user'
-    message = 'Hello, DynamoDB!'
-    response = write_message_to_dynamodb(table_name, message)
-    if response:
-        print("Message written to DynamoDB:", response)
+    
+    response1, response2 = write_message_to_dynamodb(table_name)
+    if response1 and response2:
+        print("Sample users written to DynamoDB:", response1, response2)
+    
+    print()
+    username = '15dani1'
+    item = read_message_from_dynamodb(table_name, username)
+    if item:
+        print("Message read from DynamoDB:", item)
